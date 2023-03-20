63b16cbee1aef556e2b6664b618f64b6926be99d
vdg <vdg@polygonism.net>
2023-03-20 13:39:41 +0100

    bootstrap svelte kit with standard prettier

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { getContext } from 'svelte'
+
+  import {
+    getChainDataByChainId,
+    signerAddress,
+    chainId,
+    chainData
+  } from 'svelte-ethers-store'
+
+  console.log('test')
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
