318a736c528cd48c94b53efd8adc6014e776cce3
vdg <vdg@polygonism.net>
2023-03-21 14:19:23 +0100

    basic nav home/roadmap

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -9,16 +9,14 @@
     chainId,
     chainData
   } from 'svelte-ethers-store'
-
-
 </script>
-
 
 <nav class="navbar is-white topNav">
   <div class="container">
     <div class="navbar-brand">
       <a class="navbar-item" href="../">
-        <span class="is-size-6"><b>Roadfund</b></span> <img  width="112" height="28" class="is-hidden"/>
+        <span class="is-size-6"><b>Roadfund</b></span>
+        <img width="112" height="28" class="is-hidden" />
       </a>
       <div class="navbar-burger burger" data-target="topNav">
         <span />
@@ -28,39 +26,46 @@
     </div>
     <div id="topNav" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item" href="cover.html">Home</a>
-        <a class="navbar-item" href="landing.html">Landing</a>
-        <a class="navbar-item" href="blog.html">Blog</a>
-        <a class="navbar-item" href="instaAlbum.html">Album</a>
-        <a class="navbar-item" href="kanban[search].html">Kanban</a>
-        <a class="navbar-item" href="search.html">Search</a>
-        <a class="navbar-item" href="tabs.html">Tabs</a>
+        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item" href="/0x1:0x2/">test project</a>
+        <a class="navbar-item" href="#">Menu2</a>
       </div>
       <div class="navbar-end">
         <div class="navbar-item">
-          <div class="field is-grouped">
-            <p class="control">
-              <a class="button is-small" on:click={() => blockchain.connect()}>
-                <span class="icon">
-                  <i class="fa fa-user-plus" />
-                </span>
-                <span> Connect </span>
-              </a>
-            </p>
-            <p class="control is-hidden">
-              <a class="button is-small is-info is-outlined">
-                <span class="icon">
-                  <i class="fa fa-user" />
-                </span>
-                <span>Login</span>
-              </a>
-            </p>
-          </div>
+          {#if $signerAddress}
+            {$signerAddress}
+          {:else}
+            <div class="field is-grouped">
+              <p class="control">
+                <a
+                  class="button is-small"
+                  on:click={() => blockchain.connect()}>
+                  <span class="icon">
+                    <i class="fa fa-user-plus" />
+                  </span>
+                  <span> Connect </span>
+                </a>
+              </p>
+              <p class="control is-hidden">
+                <a class="button is-small is-info is-outlined">
+                  <span class="icon">
+                    <i class="fa fa-user" />
+                  </span>
+                  <span>Login</span>
+                </a>
+              </p>
+            </div>
+          {/if}
         </div>
       </div>
     </div>
   </div>
 </nav>
+
+<section class="container mt-6">
+  <slot />
+</section>
+
 <nav class="navbar is-white is-hidden">
   <div class="container">
     <div class="navbar-menu">
@@ -77,171 +82,6 @@
     </div>
   </div>
 </nav>
-<section class="container mt-6">
-  <div class="columns">
-    <div class="column is-3">
-      <a class="button is-primary is-block is-alt is-large" href="#"
-        >New Post</a>
-      <aside class="menu">
-        <p class="menu-label">Tags</p>
-        <ul class="menu-list">
-          <li><span class="tag is-primary is-medium ">Dashboard</span></li>
-          <li><span class="tag is-link is-medium ">Customers</span></li>
-          <li>
-            <span class="tag is-light is-danger is-medium "
-              >Authentication</span>
-          </li>
-          <li><span class="tag is-dark is-medium ">Payments</span></li>
-          <li><span class="tag is-success is-medium ">Transfers</span></li>
-          <li><span class="tag is-warning is-medium ">Balance</span></li>
-          <li><span class="tag is-medium ">Question</span></li>
-        </ul>
-      </aside>
-    </div>
-    <div class="column is-9">
-      <div class="box content">
-        <article class="post">
-          <h4>Bulma: How do you center a button in a box?</h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@jsmith</a> replied 34 minutes ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 1</span>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <h4>How can I make a bulma button go full width?</h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@red</a> replied 40 minutes ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 0</span>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <h4>
-            TypeError: Data must be a string or a buffer when trying touse
-            vue-bulma-tabs
-          </h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@jsmith</a> replied 53 minutes ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 13</span>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <h4>How to vertically center elements in Bulma?</h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@brown</a> replied 3 hours ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 2</span>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <h4>
-            I'm trying to use hamburger menu on bulma css, but it doesn't work.
-            What is wrong?
-          </h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@hamburgler</a> replied 5 hours ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 2</span>
-            </div>
-          </div>
-        </article>
-        <article class="post">
-          <h4>How to make tiles wrap with Bulma CSS?</h4>
-          <div class="media">
-            <div class="media-left">
-              <p class="image is-32x32">
-                <img src="http://bulma.io/images/placeholders/128x128.png" />
-              </p>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <a href="#">@rapper</a> replied 3 hours ago &nbsp;
-                  <span class="tag">Question</span>
-                </p>
-              </div>
-            </div>
-            <div class="media-right">
-              <span class="has-text-grey-light"
-                ><i class="fa fa-comments" /> 2</span>
-            </div>
-          </div>
-        </article>
-      </div>
-    </div>
-  </div>
-</section>
 <footer class="footer">
   <div class="container">
     <div class="content has-text-centered">
@@ -271,6 +111,4 @@
 
 <style lang="scss" global>
   @import '../scss/main.scss';
-
-
 </style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -7,11 +7,6 @@
     chainId,
     chainData
   } from 'svelte-ethers-store'
-
-  console.log('test')
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+test
